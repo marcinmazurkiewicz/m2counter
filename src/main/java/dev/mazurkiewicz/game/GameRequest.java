bba376134b5d6792e3b5cd55f26a2f3c8ee68ab1
Marcin Mazurkiewicz <marcin@mazurkiewicz.dev>
2021-05-05 19:35:09 +0200

    Game request and response DTOs added

---
+++ src/main/java/dev/mazurkiewicz/game/GameRequest.java
@@ -0,0 +1,13 @@
+package dev.mazurkiewicz.game;
+
+public class GameRequest {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
