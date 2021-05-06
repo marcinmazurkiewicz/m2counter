9bc4e5e9dd205df7f84d66aa60f63616a5f17bf7
Marcin Mazurkiewicz <marcin@mazurkiewicz.dev>
2021-05-06 13:46:11 +0200

    Added game endpoint controller

---
+++ src/main/java/dev/mazurkiewicz/game/GameResource.java
@@ -0,0 +1,33 @@
+package dev.mazurkiewicz.game;
+
+import org.jboss.resteasy.annotations.jaxrs.PathParam;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import java.util.List;
+
+@Path("/games")
+public class GameResource {
+    private final GameService service;
+
+    public GameResource(GameService service) {
+        this.service = service;
+    }
+
+    @GET
+    public List<GameResponse> getAllGames() {
+        return service.getAllGames();
+    }
+
+    @GET
+    @Path("/{gameId}")
+    public GameResponse getAllGames(@PathParam Long gameId) {
+        return service.getGameById(gameId);
+    }
+
+    @POST
+    public GameResponse saveGame(GameRequest gameRequest) {
+        return service.saveGame(gameRequest);
+    }
+}
