b801ea9320d59e31a5420a58a5b66d5c2e2091b6
Marcin Mazurkiewicz <marcin@mazurkiewicz.dev>
2021-05-08 20:29:14 +0200

    Added token refresh entity and repository to it

---
+++ src/main/java/dev/mazurkiewicz/auth/TokenRepository.java
@@ -0,0 +1,34 @@
+package dev.mazurkiewicz.auth;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+import java.util.Optional;
+
+@ApplicationScoped
+public class TokenRepository {
+    private final EntityManager entityManager;
+
+    public TokenRepository(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    public Optional<RefreshToken> getRefreshToken(String token) {
+        return entityManager.createNamedQuery("RefreshTokens.findByToken", RefreshToken.class)
+                .setParameter("token", token).getResultList()
+                .stream()
+                .findFirst();
+    }
+
+    @Transactional
+    public void saveRefreshToken(RefreshToken refreshToken) {
+        entityManager.persist(refreshToken);
+    }
+
+    @Transactional
+    public void removeToken(RefreshToken token) {
+        entityManager.createNamedQuery("RefreshTokens.removeToken")
+                .setParameter("id", token.getId())
+                .executeUpdate();
+    }
+}
