dcf9d2c5b40ba9bfd031310c6ebbad84e4133237
Marcin Mazurkiewicz <marcin@mazurkiewicz.dev>
2021-05-09 10:50:23 +0200

    Added generating refresh tokens and method that prepare a response with tokens

--- src/main/java/dev/mazurkiewicz/auth/TokenService.java
+++ src/main/java/dev/mazurkiewicz/auth/TokenService.java
@@ -1,32 +1,85 @@
 package dev.mazurkiewicz.auth;
 
-import dev.mazurkiewicz.user.Role;
-import dev.mazurkiewicz.user.User;
+import dev.mazurkiewicz.user.UserResponse;
+import io.quarkus.security.UnauthorizedException;
 import io.smallrye.jwt.build.Jwt;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.NewCookie;
+import javax.ws.rs.core.Response;
 import java.time.Instant;
 import java.time.LocalDateTime;
-import java.util.stream.Collectors;
+import java.util.UUID;
 
 @ApplicationScoped
 public class TokenService {
-    private final JwtProperties jwtProperties;
+    private final TokenProperties tokenProperties;
+    private final TokenRepository tokenRepository;
 
-    public TokenService(JwtProperties jwtProperties) {
-        this.jwtProperties = jwtProperties;
+    public TokenService(TokenProperties tokenProperties, TokenRepository tokenRepository) {
+        this.tokenProperties = tokenProperties;
+        this.tokenRepository = tokenRepository;
     }
 
-    public String generateToken(User user) {
-        String token = Jwt.issuer(jwtProperties.getIssuer())
+    public Response prepareTokens(UserResponse user) {
+        NewCookie refreshCookie = new NewCookie(tokenProperties.getRefreshTokenName(), createRefreshToken(user.getId()),
+                "/", null, null, tokenProperties.getRefreshTokenExpirationAfterSeconds(),
+                false, true);
+        return Response.ok()
+                .header(HttpHeaders.AUTHORIZATION, generateJwtToken(user))
+                .cookie(refreshCookie)
+                .entity(user)
+                .build();
+    }
+
+    public RefreshToken getValidRefreshToken(String token) {
+        if (token == null)
+            throw new UnauthorizedException("Token is null");
+        RefreshToken refreshToken = tokenRepository.getRefreshToken(token)
+                .orElseThrow(() -> new UnauthorizedException("Invalid refresh token"));
+        if (isTokenExpired(refreshToken))
+            throw new UnauthorizedException("Token has expired");
+        return refreshToken;
+    }
+
+    public void removeToken(RefreshToken token) {
+        tokenRepository.removeToken(token);
+    }
+
+    private String generateJwtToken(UserResponse user) {
+        String token = Jwt.issuer(tokenProperties.getIssuer())
                 .upn(user.getEmail())
-                .groups(user.getRoles().stream().map(Role::getRole).collect(Collectors.toSet()))
+                .groups(user.getRoles())
                 .claim("uid", user.getId())
                 .issuedAt(Instant.now())
                 .expiresAt(LocalDateTime.now()
-                        .plusSeconds(jwtProperties.getTokenExpirationAfterSeconds())
-                        .atZone(jwtProperties.getTimezoneId()).toInstant())
+                        .plusSeconds(tokenProperties.getTokenExpirationAfterSeconds())
+                        .atZone(tokenProperties.getTimezoneId()).toInstant())
                 .sign();
-        return String.format("%s %s", jwtProperties.getTokenPrefix(), token);
+        return String.format("%s %s", tokenProperties.getTokenPrefix(), token);
+    }
+
+    private String createRefreshToken(Long userId) {
+        RefreshToken refreshToken = new RefreshToken();
+        refreshToken.setUserId(userId);
+        refreshToken.setRefreshToken(generateRefreshToken());
+        refreshToken.setCreatedAt(Instant.now());
+        refreshToken.setExpiredAt(LocalDateTime.now()
+                .plusSeconds(tokenProperties.getRefreshTokenExpirationAfterSeconds())
+                .atZone(tokenProperties.getTimezoneId()).toInstant());
+        tokenRepository.saveRefreshToken(refreshToken);
+        return refreshToken.getRefreshToken();
+    }
+
+    private String generateRefreshToken() {
+        return UUID.randomUUID().toString();
+    }
+
+    private boolean isTokenExpired(RefreshToken token) {
+        boolean isExpired = Instant.now().isAfter(token.getExpiredAt());
+        if (isExpired)
+            tokenRepository.removeToken(token);
+        return isExpired;
     }
 }
