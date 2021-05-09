b69429d158bb1404098d1f411ae6b294ea32c673
Marcin Mazurkiewicz <marcin@mazurkiewicz.dev>
2021-05-09 10:52:08 +0200

    Added a method to manage token refreshing

--- src/main/java/dev/mazurkiewicz/auth/AuthorizationService.java
+++ src/main/java/dev/mazurkiewicz/auth/AuthorizationService.java
@@ -1,31 +1,41 @@
 package dev.mazurkiewicz.auth;
 
-import dev.mazurkiewicz.exception.ResourceNotFoundException;
-import dev.mazurkiewicz.user.*;
+import dev.mazurkiewicz.user.User;
+import dev.mazurkiewicz.user.UserMapper;
+import dev.mazurkiewicz.user.UserResponse;
+import dev.mazurkiewicz.user.UserService;
 import dev.mazurkiewicz.util.PasswordUtils;
 import io.quarkus.security.UnauthorizedException;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.core.Response;
 
 @ApplicationScoped
 public class AuthorizationService {
-    private final UserRepository userRepository;
+    private final UserService userService;
     private final TokenService tokenService;
+    private final UserMapper userMapper;
 
-    public AuthorizationService(UserRepository userRepository, TokenService tokenService) {
-        this.userRepository = userRepository;
+    public AuthorizationService(UserService userService, TokenService tokenService, UserMapper userMapper) {
+        this.userService = userService;
         this.tokenService = tokenService;
+        this.userMapper = userMapper;
     }
 
-    public String login(LoginRequest loginRequest) {
-        User user = userRepository.findUser(loginRequest.getUsername())
-                .orElseThrow(() -> new ResourceNotFoundException(String.format("User with email %s not found", loginRequest.getUsername())));
+    public Response login(LoginRequest loginRequest) {
+        User user = userService.findUser(loginRequest.getUsername());
         if (PasswordUtils.verifyPassword(loginRequest.getPassword(), user.getPassword())) {
-            return tokenService.generateToken(user);
+            return tokenService.prepareTokens(userMapper.mapEntityToResponse(user));
         } else {
-            throw new UnauthorizedException();
+            throw new UnauthorizedException("Incorrect password");
         }
     }
 
-
+    public Response refreshToken(String token) {
+        RefreshToken refreshToken = tokenService.getValidRefreshToken(token);
+        UserResponse user = userService.findUserById(refreshToken.getUserId());
+        Response response = tokenService.prepareTokens(user);
+        tokenService.removeToken(refreshToken);
+        return response;
+    }
 }
