4bb3da8ed89112d89dfa471621ee7efb28986726
Marcin Mazurkiewicz <marcin@mazurkiewicz.dev>
2021-05-14 21:09:19 +0200

    Changes in ErrorResponse: convert ErrorInfo to List of Error info to handle multiple constraint violation for one field

--- src/main/java/dev/mazurkiewicz/exception/ConstraintViolationExceptionHandler.java
+++ src/main/java/dev/mazurkiewicz/exception/ConstraintViolationExceptionHandler.java
@@ -1,13 +1,15 @@
 package dev.mazurkiewicz.exception;
 
+import javax.validation.ConstraintViolation;
 import javax.validation.ConstraintViolationException;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Provider
 public class ConstraintViolationExceptionHandler implements ExceptionMapper<ConstraintViolationException> {
@@ -17,19 +19,27 @@ public class ConstraintViolationExceptionHandler implements ExceptionMapper<Cons
 
     @Override
     public Response toResponse(ConstraintViolationException e) {
-        Map<String, ErrorInfo> errors = e.getConstraintViolations()
-                .stream()
-                .map(ConstraintViolationDescriptor::new)
-                .collect(Collectors.toMap(
-                        ConstraintViolationDescriptor::getFieldName,
-                        ConstraintViolationDescriptor::getErrorInfo
-                        )
-                );
+        Map<String, List<ErrorInfo>> errors = mapViolationsToFieldErrorInfo(e.getConstraintViolations());
         ErrorResponse response = new ErrorResponse(Response.Status.BAD_REQUEST.getStatusCode(),
                 "Some fields are invalid", uriInfo.getPath(), ErrorType.VALIDATION_ERROR, errors);
         return Response
                 .status(Response.Status.BAD_REQUEST)
                 .entity(response)
                 .build();
+    }
+
+    private Map<String, List<ErrorInfo>> mapViolationsToFieldErrorInfo(Set<ConstraintViolation<?>> violations) {
+        return violations.stream()
+                .map(ConstraintViolationDescriptor::new)
+                .collect(Collectors.toMap(
+                        ConstraintViolationDescriptor::getFieldName,
+                        x -> Collections.singletonList(x.getErrorInfo()),
+                        this::mergeErrorInfos
+                        )
+                );
+    }
+
+    private List<ErrorInfo> mergeErrorInfos(List<ErrorInfo> first, List<ErrorInfo> second) {
+        return Stream.concat(first.stream(), second.stream()).collect(Collectors.toList());
     }
 }
