14ec59e75f027557486fda9e6160f41bd3bfdba0
Marcin Mazurkiewicz <marcin@mazurkiewicz.dev>
2021-05-15 14:32:09 +0200

    Replace id by uid in UserResponse and refactor mapper

--- src/main/java/dev/mazurkiewicz/user/UserResponse.java
+++ src/main/java/dev/mazurkiewicz/user/UserResponse.java
@@ -1,33 +1,28 @@
 package dev.mazurkiewicz.user;
 
 import java.util.Set;
+import java.util.UUID;
 
 public class UserResponse {
-    private Long id;
-    private String email;
-    private Set<String> roles;
+    private final UUID uid;
+    private final String email;
+    private final Set<String> roles;
 
-    public Long getId() {
-        return id;
+    public UserResponse(UUID uid, String email, Set<String> roles) {
+        this.uid = uid;
+        this.email = email;
+        this.roles = roles;
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    public UUID getUid() {
+        return uid;
     }
 
     public String getEmail() {
         return email;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
     public Set<String> getRoles() {
         return roles;
-    }
-
-    public void setRoles(Set<String> roles) {
-        this.roles = roles;
     }
 }
