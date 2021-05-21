b3450a9558be65498ff761723c0e3c1fed97cda9
Marcin Mazurkiewicz <marcin@mazurkiewicz.dev>
2021-05-21 13:31:02 +0200

    Changed id in Character from long to uuid

--- src/main/java/dev/mazurkiewicz/character/Character.java
+++ src/main/java/dev/mazurkiewicz/character/Character.java
@@ -1,26 +1,31 @@
 package dev.mazurkiewicz.character;
 
-import javax.persistence.*;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.NamedQuery;
 import java.util.UUID;
 
 @Entity(name = "characters")
 @NamedQuery(name = "Characters.findAllForUser",
-        query = "SELECT c FROM characters c WHERE c.uid = :uid")
+        query = "SELECT c FROM characters c WHERE c.userId = :userId")
 public class Character {
-    private Long id;
+    private UUID id;
     private String name;
     private String description;
-    private UUID uid;
+    private UUID userId;
 
     @Id
-    @SequenceGenerator(name = "characterSeq", sequenceName = "character_id_seq", allocationSize = 1)
-    @GeneratedValue(generator = "characterSeq")
-    public Long getId() {
+    @GenericGenerator(name = "UUIDGenerator", strategy = "uuid2")
+    @GeneratedValue(generator = "UUIDGenerator")
+    public UUID getId() {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    public void setId(UUID uuid) {
+        this.id = uuid;
     }
 
     public String getName() {
@@ -39,11 +44,11 @@ public class Character {
         this.description = description;
     }
 
-    public UUID getUid() {
-        return uid;
+    public UUID getUserId() {
+        return userId;
     }
 
-    public void setUid(UUID uid) {
-        this.uid = uid;
+    public void setUserId(UUID userId) {
+        this.userId = userId;
     }
 }
