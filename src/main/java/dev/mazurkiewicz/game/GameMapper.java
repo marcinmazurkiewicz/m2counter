556e90a3e1728c62f4a07ecf17141bfe300d91d2
Marcin Mazurkiewicz <marcin@mazurkiewicz.dev>
2021-05-22 13:39:54 +0200

    Added new fields to game entity and adapted dependent classes

--- src/main/java/dev/mazurkiewicz/game/GameMapper.java
+++ src/main/java/dev/mazurkiewicz/game/GameMapper.java
@@ -1,6 +1,10 @@
 package dev.mazurkiewicz.game;
 
+import dev.mazurkiewicz.character.CharacterResponse;
+
 import javax.enterprise.context.ApplicationScoped;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @ApplicationScoped
 public class GameMapper {
@@ -11,9 +15,13 @@ public class GameMapper {
     }
 
     public GameResponse mapEntityToResponse(Game entity) {
-        GameResponse response = new GameResponse();
-        response.setId(entity.getId());
-        response.setName(entity.getName());
-        return response;
+        CharacterResponse creator = new CharacterResponse(entity.getCreator().getId(), entity.getCreator().getName(), entity.getCreator().getDescription());
+        CharacterResponse gameMaster = new CharacterResponse(entity.getCreator().getId(), entity.getCreator().getName(), entity.getCreator().getDescription());
+        Set<CharacterResponse> players = entity.getPlayers()
+                .stream()
+                .map(x -> new CharacterResponse(x.getId(), x.getName(), x.getDescription()))
+                .collect(Collectors.toSet());
+        return new GameResponse(entity.getId(), entity.getName(), creator, gameMaster, players, entity.isTrustMode());
+
     }
 }
