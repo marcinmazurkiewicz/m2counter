0208fbc8281000d4473fefdd8044f66ad14d60df
Marcin Mazurkiewicz <marcin@mazurkiewicz.dev>
2021-05-22 13:58:04 +0200

    Modified save game method

--- src/main/java/dev/mazurkiewicz/game/GameService.java
+++ src/main/java/dev/mazurkiewicz/game/GameService.java
@@ -1,19 +1,28 @@
 package dev.mazurkiewicz.game;
 
+import dev.mazurkiewicz.character.Character;
+import dev.mazurkiewicz.character.CharacterRepository;
 import dev.mazurkiewicz.exception.ResourceNotFoundException;
+import io.quarkus.security.UnauthorizedException;
+import org.eclipse.microprofile.jwt.Claim;
 
 import javax.enterprise.context.ApplicationScoped;
 import java.util.List;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 @ApplicationScoped
 public class GameService {
     private final GameRepository repository;
     private final GameMapper mapper;
+    private final CharacterRepository characterRepository;
+    private final UUID userId;
 
-    public GameService(GameRepository repository, GameMapper mapper) {
+    public GameService(GameRepository repository, GameMapper mapper, CharacterRepository characterRepository, @Claim("uid") String uid) {
         this.repository = repository;
         this.mapper = mapper;
+        this.characterRepository = characterRepository;
+        this.userId = UUID.fromString(uid);
     }
 
     public List<GameResponse> getAllGames() {
@@ -30,8 +39,16 @@ public class GameService {
                 .orElseThrow(() -> new ResourceNotFoundException(String.format("Game with id %d not found", gameId)));
     }
 
-    public GameResponse saveGame(GameRequest gameRequest) {
+    public GameResponse createGame(GameRequest gameRequest) {
         Game game = mapper.mapRequestToEntity(gameRequest);
+        Character creator = characterRepository.findCharacterById(gameRequest.getCreatorId())
+                .orElseThrow(() -> new ResourceNotFoundException(String.format("Character with %s not found", gameRequest.getCreatorId())));
+        if (!creator.getUserId().equals(userId)) {
+            throw new UnauthorizedException("This character don't belong to you!");
+        }
+        game.setCreator(creator);
+        game.setGameMaster(creator);
+        game.addPlayer(creator);
         repository.saveGame(game);
         return mapper.mapEntityToResponse(game);
     }
